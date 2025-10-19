ebbc9c4b072adad0944612a41d661846c9bbed36
AhYoungJo <doubletwosep@gmail.com>
2025-10-19 18:13:57 +0900

    (feat)merge-caculator
    계산기 모듈 최종 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { calculate } from "./Calculator.js";
 class App {
-  async run() {}
+  async run() {
+    const input = await MissionUtils.Console.readLineAsync("문자열을 입력하세요: ");
+    const result = calculate(input);
+    MissionUtils.Console.print(`결과 : ${result}`);
+  }
 }
-
 export default App;
